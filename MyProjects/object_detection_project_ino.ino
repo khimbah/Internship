8f8c351f5e3dc2c0ffb001e8a2c5981220088e31
“[Khumbulani <[khumbulani@kartoza]>
2016-06-21 09:36:28 +0200

    added object_detection_project_ino.ino file for mYown.ino

--- MyProjects/object_detection_project_ino.ino
+++ MyProjects/object_detection_project_ino.ino
@@ -1,33 +1,42 @@
 //This program calculates the number of times the ball passes by the sensor as it is thrown to and fro by two or more people
-    int count = 1; //declaring a global variable
+//declaring a global variable
+int count = 1; 
+int total_count = 0;
+long  cm = 0; 
+long max_distance = 0;
+long min_distance = 0;
+long distance = 0;
+
 
 //setting pin number 7 as a constant
-    const input_signal_pin = 7;
-
+    const int input_signal_pin = 7;
+  
 //setting up function variables
     void setup(){
+      
     long signal_duration;
-    long max_distance = 400; min_distance = cm;
-    long distance = max_distance - min_distance;
+     max_distance = 400; 
+     min_distance = cm;
+     distance = max_distance - min_distance;
 
 //setting pin mode for output signal
-    pinMode(signal_in, OUTPUT);
+    pinMode(input_signal_pin, OUTPUT);
  
 //invoking the signal
-    digitalWrite(signal_in, LOW);
+    digitalWrite(input_signal_pin, LOW);
     delayMicroseconds(2);
-    digitalWrite(signal_in, HIGH);
+    digitalWrite(input_signal_pin, HIGH);
     delayMicroseconds(5);
-    digitalWrite(signal_in, LOW);
+    digitalWrite(input_signal_pin, LOW);
     
 //setting up pin mode for input signal    
-    pinMode(signal_in, INPUT);
+    pinMode(input_signal_pin, INPUT);
     
 //receiving signal from sensor    
-    signal_duration = pulseIn(signal_in, HIGH);
+    signal_duration = pulseIn(input_signal_pin, HIGH);
 //setting up methods
-    int total_count = number_of_counts(count);//Serial.onRecieve();
-    long  cm = microsecondsToCentimeters(signal_duration);
+     total_count = number_of_counts(count);//Serial.onRecieve();
+     cm = microsecondsToCentimeters(signal_duration);
 //establishing serial communication
     Serial.begin(9600);
 }
@@ -35,11 +44,12 @@
 //running the functions
 void loop(){
   if(min_distance <= 0){
-    Serial.print();
+    Serial.println();
     delay(100);
-              } else if(distance < max_distance - (400 + cm)/2) 
+              } else if( distance < max_distance - (400 + cm)/2 ) 
                       {
-                        Serial.print(total_count "counts ");
+                        Serial.print(total_count);
+                        Serial.print(" counts ");
                         Serial.println();
                         delay(100);
                       }
